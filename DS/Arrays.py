8bf07e02f6c514fa6addb53820039588070ef23d
Yogeshvar <yogeshvar@icloud.com>
2023-09-06 18:44:30 +0930

    ✨ add search, more rotate

--- DS/Arrays.py
+++ DS/Arrays.py
@@ -64,5 +64,74 @@ def rotate_one_l_v2(arr, d):
         d = d - 1
     return arr
 
+def rotate_one_l_v3(arr, d):
+    new_arr = []
+    new_start = arr.index(d)
+    new_arr = arr[new_start+1:] + arr[0:new_start+1]
+    return new_arr
+
+import math
+def rotate_one_l_v4(arr, d):
+    # juggling algo
+    for i in range(math.gcd(d, len(arr))):
+        temp = arr[i]
+        j = i
+        while 1:
+            k = j + d
+            if k >= len(arr):
+                k = k - len(arr)
+            if k == i:
+                break
+            arr[j] = arr[k]
+            j = k
+            arr[j] = temp
+    return arr
+
+def reverseArray(arr, start, end):
+    while start <= end:
+        temp = arr[end]
+        arr[end] = arr[start]
+        arr[start] = temp
+        start = start + 1
+        end = end - 1
+    return arr
+
+def rotate_one_l_v5(arr, d):
+    # reversal algorithm
+    d = d % len(arr)
+    arr = reverseArray(arr, 0, d-1)
+    arr = reverseArray(arr, d, len(arr) - 1)
+    return reverseArray(arr, 0, len(arr) - 1)
+
+    
+        
 print(rotate_one_l(array, 2)) # [5, 4, 3, 2, 1, 7, 6]
 print(rotate_one_l_v2(init_array, 2)) # [3, 4, 5, 6, 1, 2]
+print(rotate_one_l_v3(init_array, 2)) # [3, 4, 5, 6, 1, 2]
+print(rotate_one_l_v4(init_array, 2)) # [5, 6, 1, 2, 3, 4]
+print(rotate_one_l_v5(init_array, 2)) # [1, 2, 3, 4, 5, 6]
+
+def insert_elem(arr, elem, pos):
+    arr.insert(pos, elem)
+    return arr
+    
+print(insert_elem(init_array, 7, 7)) # [1, 2, 3, 4, 5, 6, 7]
+
+def delete_elem(arr, elem):
+    arr.remove(elem)
+    return arr
+
+print(delete_elem(init_array, 7)) # [1, 2, 3, 4, 5, 6]
+
+def binary_search(arr, low, high, key):
+    if high < low:
+        return -1
+    mid = (low + high) // 2
+    if key == arr[mid]:
+        return mid
+    if key > arr[mid]:
+        return binary_search(arr, mid + 1, high, key)
+    return binary_search(arr, low, mid - 1, key)
+
+print(binary_search(init_array, 0, len(init_array) - 1, 9)) # -1
+print(binary_search(init_array, 0, len(init_array) - 1, 3)) # 2
